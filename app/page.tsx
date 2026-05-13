1b403ed410391d1059c2c832b463be7e003f9a4b
unknown <ms3071687@gmail.com>
2026-05-13 17:01:20 +0500

    implement navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="text-indigo-600 text-5xl">Crypto Pulse</div>;
+  return (
+    <>
+      <div></div>
+    </>
+  );
 };
 
 export default Home;
